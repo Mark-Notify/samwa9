3ba5269d1e91d9c9baae7d618b9b819729e3eb0e
Mark <diaryads9@gmail.com>
2019-09-08 13:08:28 +0700

    sad

--- application/helpers/fn_helper.php
+++ application/helpers/fn_helper.php
@@ -2,14 +2,8 @@
 
 function DateThai($strDate)
 {
-	$strYear = date("Y",strtotime($strDate))+543;
-	$strMonth= date("m",strtotime($strDate));
-	$strDay= date("j",strtotime($strDate));
-	$strHour= date("H",strtotime($strDate));
-	$strMinute= date("i",strtotime($strDate));
-	$strSeconds= date("s",strtotime($strDate));
 	$strMonthCut = Array("","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค.");
-	$strMonthThai=$strMonthCut[$strMonth];
+	$strMonthThai=$strMonthCut[$strDate];
 	return "$strMonthThai";
 }
 
